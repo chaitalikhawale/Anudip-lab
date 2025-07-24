327932fddf747ed44f3bb16aab3dfc5bd5aa1d63
chaitalikhawale <chaitalikhawale52@gmail.com>
2025-07-24 11:40:12 +0530

    Add files via upload

---
+++ copycontent.js
@@ -0,0 +1,19 @@
+//Question 1 : Write a program to copy the contents of a file named source.txt to a new file named 
+//destination.txt. Check if the source file exists before copying. 
+
+const fs = require('fs');
+// Adding the content to the source txt to get copied to destination-txt
+fs.appendFileSync('source-txt','Hello students have a nice day','utf-8');
+
+// Check if source.txt exists
+if (fs.existsSync('source-txt',)) {
+    // Read the contents of source.txt
+    const data = fs.readFileSync('source-txt', 'utf-8');
+
+    // Write the data to destination.txt
+    fs.writeFileSync('destination-txt', data);
+
+    console.log("File copied successfully!");
+} else {
+    console.log("source-txt does not exist.");
+}
